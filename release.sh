59b2e63d34a5e1fd42c7e94e99ee865fc0ebfd22
Chito Wong <chi.to.wong@outlook.com>
2022-01-15 16:07:42 +0800

    Update release workflow

--- release.sh
+++ release.sh
@@ -1,12 +1,22 @@
+#!/bin/bash
+set -eux
+
 echo "App name: $1"
 echo "Version: $2"
 echo "Environment: $3"
 
+# Checkout to gh-pages branch
 cd TARGET_REPO/
 { git checkout gh-pages || git checkout -b gh-pages }
+
+# Clear folder
 cd ..
 rm -rf TARGET_REPO/*
+
+# Copy artifacts
 cp -r ./$1/$2/* TARGET_REPO/
+
+# Push
 cd TARGET_REPO
 git add .
 git commit -m "Release version $2 to $3"
