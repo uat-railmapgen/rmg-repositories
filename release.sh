439bbc1ba43afcb6680645e47fbed959488cced1
Chito Wong <chi.to.wong@outlook.com>
2022-01-15 16:04:54 +0800

    Update release workflow

--- release.sh
+++ release.sh
@@ -1,1 +1,13 @@
-echo $GITHUB_CONTEXT
+echo "App name: $1"
+echo "Version: $2"
+echo "Environment: $3"
+
+cd TARGET_REPO/
+{ git checkout gh-pages || git checkout -b gh-pages }
+cd ..
+rm -rf TARGET_REPO/*
+cp -r ./$1/$2/* TARGET_REPO/
+cd TARGET_REPO
+git add .
+git commit -m "Release version $2 to $3"
+git push -u origin gh-pages
