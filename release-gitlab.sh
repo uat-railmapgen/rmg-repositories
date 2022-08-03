26c44715fa820aeb4364982ae5015954bfa3b2e2
Chito Wong <chi.to.wong@outlook.com>
2022-08-04 01:03:00 +0800

    Update release script for GitLab

--- release-gitlab.sh
+++ release-gitlab.sh
@@ -9,13 +9,14 @@ cd GITLAB_REPO/
 
 # Clear artefact folder
 rm -rf build
-mkdir build
+rm -rf public
+mkdir public
 
 # Copy artifacts
-cp -r ../"$1"/"$2"/* build/
+cp -r ../"$1"/"$2"/* public/
 
 ### WRITE INFO.JSON
-cat >build/info.json <<EOF
+cat >public/info.json <<EOF
 {
   "component": "$1",
   "version": "$2",
