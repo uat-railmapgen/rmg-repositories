db95b6c92d11f283106103d2cd630e7eba14dd4b
Chito Wong <chi.to.wong@outlook.com>
2022-11-28 20:55:37 +0800

    rmg-home housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -3,16 +3,25 @@ set -eux
 
 # ls | sort -V
 
-app="rmg-components"
+app="railmapgen.github.io"
 versionList=(
-4.0.0
-4.3.0
-4.3.1
-4.3.2
-4.3.3
-4.3.4
-4.3.5
-4.3.6
+0.1.4
+0.1.5
+0.1.5.#4.2ee16ad
+0.1.6
+0.1.7
+0.2.0
+0.2.1
+0.2.2
+0.2.3
+0.2.4
+0.2.5
+0.2.6
+0.2.7
+0.2.8
+0.2.9
+0.2.10
+0.2.11
 )
 
 for version in ${versionList[*]}
