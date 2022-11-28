2cc37229e12b0ff1bf053b9104f1e1d02e13d59d
Chito Wong <chi.to.wong@outlook.com>
2022-11-28 18:26:34 +0800

    rmg-components housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -3,20 +3,16 @@ set -eux
 
 # ls | sort -V
 
-app="rmg-palette"
+app="rmg-components"
 versionList=(
-0.5.0
-0.5.0-0.#97.62b342f
-0.5.3
-0.5.5
-0.6.0
-0.6.2
-0.6.4
-0.6.5
-0.6.9
-0.6.14
-0.6.15
-0.6.17
+4.0.0
+4.3.0
+4.3.1
+4.3.2
+4.3.3
+4.3.4
+4.3.5
+4.3.6
 )
 
 for version in ${versionList[*]}
