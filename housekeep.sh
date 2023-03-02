67ddcebc9603947f11ee27f26a6178d4117764b9
Chito Wong <chi.to.wong@outlook.com>
2023-03-03 01:32:19 +0800

    rmg-translate housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -3,13 +3,21 @@ set -eux
 
 # ls | sort -V
 
-app="rmg-components"
+app="rmg-translate"
 versionList=(
-6.0.0
-6.0.1
-6.1.0
-6.1.1
-6.1.2
+0.0.3
+0.1.0
+0.2.0
+0.2.1
+0.2.2
+0.2.3
+0.3.0
+0.3.1
+0.4.0
+0.4.1
+1.0.1
+1.1.0
+1.1.1
 )
 
 for version in ${versionList[*]}
