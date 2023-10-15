0b9295cfcad0ccc74c16a5af38c9d05c9d3f4eba
Chito Wong <chi.to.wong@outlook.com>
2023-10-15 23:26:18 +0800

    rmp-gallery housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -3,18 +3,22 @@ set -eux
 
 # ls | sort -V
 
-app="rmg-components"
+app="rmp-gallery"
 versionList=(
-8.0.0
-8.1.0
-8.2.0
-8.2.1
-8.2.2
+1.5.0.tar.gz
+1.5.1.tar.gz
+1.5.3.tar.gz
+1.5.4.tar.gz
+1.5.5.tar.gz
+1.5.6.tar.gz
+1.5.7.tar.gz
+1.5.8.tar.gz
+1.5.9.tar.gz
 )
 
 for version in ${versionList[*]}
 do
-  rm -rf $app/$version
+  rm -f $app/$version
 done
 
 # Push
