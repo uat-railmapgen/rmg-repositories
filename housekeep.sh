08277ea59d3e484b6b24938c33a2de1d7b491152
Chito Wong <chi.to.wong@outlook.com>
2023-12-17 15:20:14 +0800

    rmp-gallery housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -5,11 +5,31 @@ set -eux
 
 app="rmp-gallery"
 versionList=(
-1.3.27.tar.gz
-1.3.28.#136-Filter-and-sort.4805c7b.tar.gz
-1.5.2.tar.gz
-1.5.20.tar.gz
-1.5.21.tar.gz
+1.6.0.tar.gz
+1.6.1.tar.gz
+1.6.10.tar.gz
+1.6.11.tar.gz
+1.6.12.tar.gz
+1.6.13.tar.gz
+1.6.14.tar.gz
+1.6.15.tar.gz
+1.6.16.tar.gz
+1.6.17.tar.gz
+1.6.18.tar.gz
+1.6.19.tar.gz
+1.6.2.tar.gz
+1.6.20.tar.gz
+1.6.21.tar.gz
+1.6.3.tar.gz
+1.6.4.tar.gz
+1.6.5.tar.gz
+1.6.6.tar.gz
+1.6.7.tar.gz
+1.6.8.tar.gz
+1.6.9.tar.gz
+1.7.0.tar.gz
+1.7.1.tar.gz
+1.7.2.tar.gz
 )
 
 for version in ${versionList[*]}
