54348273f262d75ac026326734d71331a61f2f5e
Chito Wong <chi.to.wong@outlook.com>
2023-12-17 21:52:49 +0800

    rmg-templates housekeeping

--- housekeep.sh
+++ housekeep.sh
@@ -3,33 +3,33 @@ set -eux
 
 # ls | sort -V
 
-app="rmp-gallery"
+app="rmg-templates"
 versionList=(
-1.6.0.tar.gz
-1.6.1.tar.gz
-1.6.10.tar.gz
-1.6.11.tar.gz
-1.6.12.tar.gz
-1.6.13.tar.gz
-1.6.14.tar.gz
-1.6.15.tar.gz
-1.6.16.tar.gz
-1.6.17.tar.gz
-1.6.18.tar.gz
-1.6.19.tar.gz
-1.6.2.tar.gz
-1.6.20.tar.gz
-1.6.21.tar.gz
-1.6.3.tar.gz
-1.6.4.tar.gz
-1.6.5.tar.gz
-1.6.6.tar.gz
-1.6.7.tar.gz
-1.6.8.tar.gz
-1.6.9.tar.gz
-1.7.0.tar.gz
-1.7.1.tar.gz
-1.7.2.tar.gz
+1.0.0.tar.gz
+1.0.1.tar.gz
+1.0.10.tar.gz
+1.0.11.tar.gz
+1.0.12.tar.gz
+1.0.13.tar.gz
+1.0.14.tar.gz
+1.0.15.tar.gz
+1.0.16.tar.gz
+1.0.17.tar.gz
+1.0.18.tar.gz
+1.0.19.tar.gz
+1.0.2.tar.gz
+1.0.20.tar.gz
+1.0.21.tar.gz
+1.0.22.tar.gz
+1.0.23.tar.gz
+1.0.24.tar.gz
+1.0.3.tar.gz
+1.0.4.tar.gz
+1.0.5.tar.gz
+1.0.6.tar.gz
+1.0.7.tar.gz
+1.0.8.tar.gz
+1.0.9.tar.gz
 )
 
 for version in ${versionList[*]}
